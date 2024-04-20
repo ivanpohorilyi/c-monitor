a41174ac3693b20738caa74c15a7f81a065144f9
ivanpohorilyi <thepatrickmaps@gmail.com>
2024-04-20 20:06:19 +0300

    ROUTING

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,21 +1,9 @@
 from fastapi import FastAPI
-
-from src.http_client import CMCHTTPClient
-from src.config import settings
+from src.router import router as router_crypto 
 
 
 app = FastAPI()
 
-cmc_client = CMCHTTPClient(
-    base_url="https://pro-api.coinmarketcap.com",
-    api_key=settings.CMC_API_KEY
-)
-
-@app.get("/cryptocurrencies")
-async def get_cryptocurrencies():
-    return await cmc_client.get_listings()
+app.include_router(router_crypto)
 
 
-@app.get("/cryptocurrencies/{currency_id}")
-async def get_cryptocurrency(currency_id: int):
-    return await cmc_client.get_currency(currency_id)
